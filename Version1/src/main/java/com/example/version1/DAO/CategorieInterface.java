f1f80b1b31a04a6ae7ff83c1dc0fc2a4ebd631b4
TENEDE Tene <tenedetene@gmail.com>
2022-01-09 20:41:00 +0100

    Implantation du service de messagerie au frontEnd et au backend

--- Version1/src/main/java/com/example/version1/DAO/CategorieInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.version1.DAO;
-
-import com.example.version1.entities.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieInterface extends JpaRepository<Categorie,Integer> {
-}
